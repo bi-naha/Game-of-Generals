c534c277a07d9a278755ae7735571472834a8bcf
unknown <fatemehazimi200130@gmail.com>
2022-03-31 00:54:51 -0700

    check input validation

--- src/cli/CommandLineAgent.java
+++ src/cli/CommandLineAgent.java
@@ -5,14 +5,17 @@ import model.Board;
 import model.Cell;
 import model.GameState;
 
+import static model.pieces.PieceType.contains;
+
 public class CommandLineAgent {
     private LogicalAgent logicalAgent;
+
 
     public CommandLineAgent(LogicalAgent logicalAgent) {
         this.logicalAgent = logicalAgent;
     }
 
-    public static void printBoard() {
+    public void printBoard() {
         Board board = Board.getInstance();
         Cell[][] cells = board.getCells();
 
@@ -27,22 +30,47 @@ public class CommandLineAgent {
         }
     }
 
+    public boolean isValidInput(String input) {
+        String[] result = input.split(" ");
+        if (result.length!=3) {
+            return false;
+        }
+        String type = result[0];
+        if (!contains(type)) {
+            return false;
+        }
+        int initialColumn = Integer.parseInt(result[1].substring(0, 1));
+        int initialRow = Integer.parseInt(result[1].substring(1,2));
+        if (initialColumn < 0 || initialColumn > 5 || initialRow < 0 || initialRow > 5) {
+            return false;
+        }
+        if ((initialColumn == 0 && initialRow != 0) || (initialRow == 0 && initialColumn != 0)) {
+            return false;
+        }
+        int destinationColumn = Integer.parseInt(result[2].substring(0, 1));
+        int destinationRow = Integer.parseInt(result[2].substring(1,2));
+        if (destinationColumn <= 0 || destinationColumn > 5 || destinationRow <= 0 || destinationRow > 5) {
+            return false;
+        }
+        return true;
+    }
+
     public void execution() {
         while (!GameState.getInstance().isEndGame()) {
             printBoard();
 
             String input = MyScanner.getScanner().scan();
-            //TODO CHECK VALIDATION OF INPUT =)
-            String[] result = input.split(" ");
-            String type = result[0];
-            int initialColumn = Integer.parseInt(result[1].substring(0, 1));
-            int initialRow = Integer.parseInt(result[1].substring(1,2));
-            int destinationColumn = Integer.parseInt(result[2].substring(0, 1));
-            int destinationRow = Integer.parseInt(result[2].substring(1,2));
-
-            this.logicalAgent.apply(type, initialRow, initialColumn, destinationRow, destinationColumn);
+            if (isValidInput(input)) {
+                String[] result = input.split(" ");
+                String type = result[0];
+                int initialColumn = Integer.parseInt(result[1].substring(0, 1));
+                int initialRow = Integer.parseInt(result[1].substring(1,2));
+                int destinationColumn = Integer.parseInt(result[2].substring(0, 1));
+                int destinationRow = Integer.parseInt(result[2].substring(1,2));
+                this.logicalAgent.apply(type, initialRow, initialColumn, destinationRow, destinationColumn);
+            }
         }
-        System.out.println("black win!");
 
+        System.out.println("black win!");
     }
 }
--- src/model/pieces/PieceType.java
+++ src/model/pieces/PieceType.java
@@ -2,5 +2,17 @@ package model.pieces;
 
 public enum PieceType {
     K, G, S, B, L, P,
-    k, g, s, b, l, p
+    k, g, s, b, l, p;
+
+    public static boolean contains(String type) {
+
+        for (PieceType c : PieceType.values()) {
+            if (c.name().equals(type)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
+
